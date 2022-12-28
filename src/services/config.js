650fabb0b0caea61baffcae77162c106fb891e48
Huzaifa Aslam <huzaifaslam45@gmail.com>
2022-12-29 00:00:16 +0500

    move firebase config from env to config file

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,19 @@
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDwqIAFKalKORy4T0MKdH-Om5oWSqcBmoo",
+  authDomain: "e-commerce-b4804.firebaseapp.com",
+  databaseURL: "https://e-commerce-b4804-default-rtdb.firebaseio.com",
+  projectId: "e-commerce-b4804",
+  storageBucket: "e-commerce-b4804.appspot.com",
+  messagingSenderId: "1068452350053",
+  appId: "1:1068452350053:web:87a7c13a6d87a6896d1e8c",
 };
-
 export default firebaseConfig;
